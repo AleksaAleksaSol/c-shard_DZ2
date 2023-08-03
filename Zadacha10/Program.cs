fd4b2c4156fb25752041c1b4e30d0c172d9238f1
Aleksander <i@solovev-pro.ru>
2023-08-03 21:27:45 +0300

    Решена задача №10

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число: ");
+string strNum = Console.ReadLine();
+int num = int.Parse(strNum);
+
+while (num < 100 || num > 999)
+{
+    Console.WriteLine("Число не трехзначное");
+    Console.WriteLine("Введите трехзначное число: ");
+    strNum = Console.ReadLine();
+    num = int.Parse(strNum);
+
+}
+
+num = ((num % 100) - (num % 10)) / 10;
+Console.WriteLine($"Вторе число в данном трехзначном числе {num}");
